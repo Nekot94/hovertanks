501ea8611170426d97f41aa5353aa00796096f6c
Nekot94 <nekomato9@gmail.com>
2017-03-16 21:08:00 +0300

    war

---
+++ Assets/Scripts/UI/UIDirectionScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIDirectionScript : MonoBehaviour {
+
+	public bool useRelativeRotation = true;
+
+    private Quaternion relativeRotation;
+
+	void Start () {
+        relativeRotation = transform.parent.localRotation;
+	}
+	
+	void Update () {
+        if (useRelativeRotation)
+            transform.rotation = relativeRotation;
+
+	}
+}
