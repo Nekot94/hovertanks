b736cffe516289677d9cf7fa9218762f4f8d5682
Nekot94 <nekomato9@gmail.com>
2017-03-24 17:21:15 +0300

    Add files via upload

---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public int numRoundsToWin = 5;
+    public float startDelay = 1.5f;
+    public float endDelay = 1.5f;
+    public CameraControl cameraControl;
+    public Text messageText;
+    public GameObject tankPrefab;
+    public TankManager[] tanks;
+
+    private int toundNumber;
+    private WaitForSeconds startWait;
+    private WaitForSeconds endWait;
+    private TankManager roundWinner;
+    private TankManager gameWinner;
+
+    private void Start()
+    {
+        startWait = new WaitForSeconds(startDelay);
+        endWait = new WaitForSeconds(endDelay);
+
+        SpawnAllTanks();
+        //SetCameraTargets();
+
+        //StartCoroutine(GameLoop());
+
+    }
+
+    private void SpawnAllTanks()
+    {
+        for (int i = 0;i < tanks.Length;i++)
+        {
+            tanks[i].instance = Instantiate(tankPrefab,tanks[i].spawnPoint.position,tanks[i].spawnPoint.rotation);
+            tanks[i].playerNumber = i + 1;
+            tanks[i].Setup();
+        }
+    }
+
+}
